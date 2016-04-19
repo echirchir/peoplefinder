d82afdd0544c3529f01a285ffe85765b11020c4e
Elisha <elisha.java@gmail.com>
2016-04-19 18:10:43 -0500

    Added some more code for twitter

--- facefinder/urls.py
+++ facefinder/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    url('^people/', views.home),
-    url('^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^people/', views.home, name='home'),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='index'),
     url(r'^admin/', admin.site.urls),
 ]
--- facefinder/views.py
+++ facefinder/views.py
@@ -1,9 +1,18 @@
 
 from django.shortcuts import render
+
 import tweepy
 
 def home(request):
 
+    urls = []
+
+    image_urls = search(urls)
+
+    # print(len(image_urls))
+    return render(request, 'home.html', {'image_urls': image_urls})
+
+def search(url_list):
     consumer_key = 'gqdtIop6EyPtU0kWRb5sqehhd'
     consumer_secret = '9gGJOsfmCaFt5RpxYIy1YTyb7UKBO5sL1qc8Leopa3oaYi3ZPn'
     access_token = '1564127736-UGHvrFKk6XUIh2tSrDW4FtKS5fLj4qxZpguwWHM'
@@ -16,13 +25,12 @@ def home(request):
     # Creation of the actual interface, using authentication
     api = tweepy.API(auth)
 
-    search_results = api.search('q=#desaparecidosEC')
-
-    image_urls = []
+    search_results = api.search("q='#desaparecidosEC'")
 
     for result in search_results:
 
-        if result.entities['media'] is not None:
-            image_urls.append(result.entities['media'][0]['media_url'])
+        url_list.append(result.entities['media'][0]['media_url'])
+        # if result.entities['media'] is not None:
+            # url_list.append(result.entities['media'][0]['media_url'])
 
-    return render(request, 'home.html', {'image_urls': image_urls})
+    return url_list
